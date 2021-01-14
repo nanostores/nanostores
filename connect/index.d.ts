5586d5f4f0a66e93b70828e4e35d7fa9f662213b
Andrey Sitnik <andrey@sitnik.ru>
2021-01-14 08:52:55 -0500

    Add connect() example

--- connect/index.d.ts
+++ connect/index.d.ts
@@ -14,11 +14,22 @@ interface Connect {
 }
 
 /**
+ * Bind other stores changes to this one.
  *
  * ```js
- * import { LocalStore, connect } from '@logux/state'
+ * import { LocalStore, connect, destroy } from '@logux/state'
  *
+ * import { CurrentTime } from '../store'
  *
+ * export ThisStore extends LocalStore {
+ *   constructor () {
+ *     this.start = Date.now()
+ *     this.since = 0
+ *     this[destroy] = connect(this, [CurrentTime], now => ({
+ *       since: this.start - now.value
+ *     }))
+ *   }
+ * }
  * ```
  *
  * @param current The current store.
