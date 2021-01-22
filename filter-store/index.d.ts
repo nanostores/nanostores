c6b0bfcb8d9fa845d0109e5dd83780a3eac3b41c
Andrey Sitnik <andrey@sitnik.ru>
2021-01-21 19:30:43 -0500

    Add FilterStore TypeDoc

--- filter-store/index.d.ts
+++ filter-store/index.d.ts
@@ -12,10 +12,34 @@ export type Filter<S extends SyncMap> = {
 }
 
 /**
+ * Store to load list of `SyncMap` with simple key-value requirements.
  *
+ * It will look for stores in loaded cache, log (for offline maps) and will
+ * subscribe to list from server (for remote maps).
+ *
+ * ```js
+ * import { FilterStore, loading } from '@logux/state'
+ *
+ * import { User } from '../store'
+ *
+ * let users = FilterStore.filter(User, { projectId })
+ * await users[loading]
+ * console.log(users.list)
+ * ```
  */
 export class FilterStore<M extends SyncMap> extends ClientLogStore {
   /**
+   * Shortcut to load store and start filtering.
+   *
+   * ```js
+   * import { FilterStore, loading } from '@logux/state'
+   *
+   * import { User } from '../store'
+   *
+   * let users = FilterStore.filter(User, { projectId })
+   * await users[loading]
+   * console.log(users.list)
+   * ```
    *
    * @param StoreClass
    * @param filter
@@ -33,11 +57,4 @@ export class FilterStore<M extends SyncMap> extends ClientLogStore {
    * Filtered items.
    */
   list: M[]
-
-  /**
-   *
-   * @param StoreClass
-   * @param filter
-   */
-  filter (StoreClass: ClientLogStoreConstructor<M>, filter?: Filter<M>): void
 }
