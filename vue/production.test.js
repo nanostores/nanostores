6962d13584bd947136b742817c4af8ac0a138157
Eduard Aksamitov <e@edaks.co>
2021-02-23 18:25:34 +0300

    test(vue): add production tests

---
+++ vue/production.test.js
@@ -0,0 +1,45 @@
+import { TestClient } from '@logux/client'
+import VueTesting from '@testing-library/vue'
+import Vue, { defineComponent } from 'vue'
+
+import '../test/set-production.js'
+import { install, useStore, ChannelErrors } from './index.js'
+import { defineSyncMap } from '../index.js'
+
+let { render, screen } = VueTesting
+let { h, nextTick } = Vue
+
+let Store = defineSyncMap('test')
+
+let IdTest = defineComponent(() => {
+  let store = useStore(Store, 'ID')
+  return () => h('div', store.value.isLoading ? 'loading' : store.value.id)
+})
+
+async function getText (component) {
+  let client = new TestClient('10')
+  render(
+    defineComponent(() => () =>
+      h('div', { 'data-testid': 'test' }, h(component))
+    ),
+    {
+      global: {
+        plugins: [[{ install }, client]]
+      }
+    }
+  )
+  await nextTick()
+  return screen.getByTestId('test').textContent
+}
+
+it('does not have ChannelErrors check in production mode', async () => {
+  expect(
+    await getText(
+      defineComponent(() => () =>
+        h(ChannelErrors, null, {
+          default: () => h(IdTest)
+        })
+      )
+    )
+  ).toEqual('loading')
+})
