21f02901b49da1cc99b6443e18bcbe8df23b8eef
Andrey Sitnik <andrey@sitnik.ru>
2021-02-24 10:21:45 -0500

    Add prepareForTest()

---
+++ prepare-for-test/index.js
@@ -0,0 +1,24 @@
+import { nanoid } from 'nanoid/non-secure'
+
+export function prepareForTest (client, Builder, value) {
+  let { id, ...keys } = value
+  if (!id) {
+    if (Builder.plural) {
+      id = `${Builder.plural}:${Object.keys(Builder.cache).length + 1}`
+    } else {
+      id = nanoid(6)
+    }
+  }
+
+  let store = Builder(id, client)
+  store.listen(() => {})
+
+  if ('isLoading' in store.value) {
+    store.setKey('isLoading', false)
+  }
+  for (let key in keys) {
+    store.setKey(key, keys[key])
+  }
+
+  return store
+}
