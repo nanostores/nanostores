e137f2258b1d10ea083c549e74f086001b161317
Andrey Sitnik <andrey@sitnik.ru>
2021-08-17 23:52:24 +0200

    Add STORE_CLEAN_DELAY

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ export {
   allEffects,
   effect
 } from './effect/index.js'
+export { createStore, STORE_CLEAN_DELAY } from './create-store/index.js'
 export { clean, cleanStores } from './clean-stores/index.js'
 export { update, updateKey } from './update/index.js'
 export { createDerived } from './create-derived/index.js'
-export { createStore } from './create-store/index.js'
 export { keepActive } from './keep-active/index.js'
 export { defineMap } from './define-map/index.js'
 export { createMap } from './create-map/index.js'
