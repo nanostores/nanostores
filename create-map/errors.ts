ac0772956f7d82f51c68db029b35a64bc16a8051
Andrey Sitnik <andrey@sitnik.ru>
2021-09-06 00:44:32 +0200

    Clean up type test

--- create-map/errors.ts
+++ create-map/errors.ts
@@ -1,6 +1,6 @@
 import { createMap } from '../index.js'
 
-type TestType = 
+type TestType =
   | { id: string; isLoading: true }
   | { isLoading: false; a: string; b: number }
 
@@ -9,33 +9,33 @@ let test = createMap<TestType>()
 test.subscribe((_, changedKey) => {
   if (changedKey === 'a') {
   }
-  // THROWS This condition will always return 'false' since the types '"id" | "b" | "a" | "isLoading" | undefined' and '"c"' have no overlap.
-  if (changedKey === 'c') {
+  // THROWS always return 'false'
+  if (changedKey === 'z') {
   }
 })
 
 test.listen((_, changedKey) => {
   if (changedKey === 'a') {
   }
-  // THROWS This condition will always return 'false' since the types '"id" | "b" | "a" | "isLoading"' and '"c"' have no overlap.
-  if (changedKey === 'c') {
+  // THROWS always return 'false'
+  if (changedKey === 'z') {
   }
 })
 
-test.setKey('isLoading', true);
-test.setKey('id', '123');
-// THROWS Argument of type '"c"' is not assignable to parameter of type '"id" | "b" | "a" | "isLoading"'.
-test.setKey('c', '123');
+test.setKey('isLoading', true)
+test.setKey('id', '123')
+// THROWS Argument of type '"z"' is not assignable to parameter
+test.setKey('z', '123')
 
-test.setKey('isLoading', false);
+test.setKey('isLoading', false)
 test.setKey('a', 'string')
 test.setKey('b', 5)
-// THROWS Argument of type '"c"' is not assignable to parameter of type '"id" | "b" | "a" | "isLoading"'.
-test.setKey('c', '123')
+// THROWS Argument of type '"z"' is not assignable to parameter
+test.setKey('z', '123')
 
 test.notify('isLoading')
 test.notify('id')
 test.notify('a')
 test.notify('b')
-// THROWS Argument of type '"c"' is not assignable to parameter of type '"id" | "b" | "a" | "isLoading"'.
-test.notify('c')
+// THROWS Argument of type '"z"' is not assignable to parameter
+test.notify('z')
