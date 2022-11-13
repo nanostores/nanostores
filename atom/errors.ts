8cfaad0b1140acfa93f4b117bdc24307019ece51
Andrey Sitnik <andrey@sitnik.ru>
2022-11-13 15:09:42 +0100

    Clean up type tests

--- atom/errors.ts
+++ atom/errors.ts
@@ -26,3 +26,12 @@ store2.set("new")
 
 // THROWS Expected 1 arguments, but got 0
 let store3 = atom<string>()
+
+declare const scoreSym:unique symbol
+export type scoreType = number&{ [scoreSym]:any }
+
+let parent = atom<scoreType>(1 as scoreType)
+parent.subscribe(value => {
+  value = 1 as scoreType
+  return value + 1
+})
