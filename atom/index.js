04644065238079251b974537cdae794dc88e0f26
Bogdan Chadkin <trysound@yandex.ru>
2023-05-09 13:59:24 +0700

    Store atom state in context
    Ref https://github.com/nanostores/nanostores/issues/171
    
    Here's initial attempt to move all stores state into global context
    object. Context is basically a listeners queue and a map of stores
    states.
    
    Context management api is still yet to explore. For now it's for
    internal usage now.

--- atom/index.js
+++ atom/index.js
@@ -1,33 +1,38 @@
 import { clean } from '../clean-stores/index.js'
-
-let listenerQueue = []
+import { getListenerQueue, getStoreState } from '../context/index.js'
 
 export let atom = (initialValue, level) => {
-  let listeners = []
   let store = {
-    lc: 0,
     l: level || 0,
-    value: initialValue,
+    iv: initialValue,
+    get lc() {
+      let state = getStoreState(store)
+      return state.lc
+    },
     set(data) {
-      if (store.value !== data) {
-        store.value = data
+      let state = getStoreState(store)
+      if (state.v !== data) {
+        state.v = data
         store.notify()
       }
     },
     get() {
-      if (!store.lc) {
-        store.listen(() => {})()
+      let state = getStoreState(store)
+      if (!state.lc) {
+        store.listen(() => { })()
       }
-      return store.value
+      return state.v
     },
     notify(changedKey) {
+      let state = getStoreState(store)
+      let listenerQueue = getListenerQueue()
       let runListenerQueue = !listenerQueue.length
-      for (let i = 0; i < listeners.length; i += 2) {
+      for (let i = 0; i < state.ls.length; i += 2) {
         listenerQueue.push(
-          listeners[i],
-          store.value,
+          state.ls[i],
+          state.v,
           changedKey,
-          listeners[i + 1]
+          state.ls[i + 1]
         )
       }
 
@@ -56,30 +61,33 @@ export let atom = (initialValue, level) => {
       }
     },
     listen(listener, listenerLevel) {
-      store.lc = listeners.push(listener, listenerLevel || store.l) / 2
+      let state = getStoreState(store)
+      state.lc = state.ls.push(listener, listenerLevel || store.l) / 2
 
       return () => {
-        let index = listeners.indexOf(listener)
+        let index = state.ls.indexOf(listener)
         if (~index) {
-          listeners.splice(index, 2)
-          store.lc--
-          if (!store.lc) store.off()
+          state.ls.splice(index, 2)
+          state.lc--
+          if (!state.lc) store.off()
         }
       }
     },
     subscribe(cb, listenerLevel) {
+      let state = getStoreState(store)
       let unbind = store.listen(cb, listenerLevel)
-      cb(store.value)
+      cb(state.v)
       return unbind
     },
-    off() {} /* It will be called on last listener unsubscribing.
+    off() { } /* It will be called on last listener unsubscribing.
                 We will redefine it in onMount and onStop. */
   }
 
   if (process.env.NODE_ENV !== 'production') {
     store[clean] = () => {
-      listeners = []
-      store.lc = 0
+      let state = getStoreState(store)
+      state.ls = []
+      state.lc = 0
       store.off()
     }
   }
--- deep-map/index.js
+++ deep-map/index.js
@@ -1,4 +1,5 @@
 import { atom } from '../atom/index.js'
+import { getStoreState } from '../context/index.js'
 import { getPath, setPath } from './path.js'
 
 export { getPath, setPath } from './path.js'
@@ -6,8 +7,9 @@ export { getPath, setPath } from './path.js'
 export function deepMap(initial = {}) {
   let store = atom(initial)
   store.setKey = (key, value) => {
-    if (getPath(store.value, key) !== value) {
-      store.value = setPath(store.value, key, value)
+    let state = getStoreState(store)
+    if (getPath(state.v, key) !== value) {
+      state.v = setPath(state.v, key, value)
       store.notify(key)
     }
   }
