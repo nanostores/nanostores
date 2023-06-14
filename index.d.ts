9ae128ec6afc52a8b017755a3c2cd5372c0d4855
Andrey Sitnik <andrey@sitnik.ru>
2023-06-14 17:12:18 +0200

    Sort exports

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,17 @@
+export { action, lastAction } from './action/index.js'
+export { atom, Atom, ReadableAtom, WritableAtom } from './atom/index.js'
+export { clean, cleanStores } from './clean-stores/index.js'
+export { computed } from './computed/index.js'
+export {
+  AllPaths,
+  BaseDeepMap,
+  deepMap,
+  DeepMapStore,
+  FromPath,
+  getPath,
+  setPath
+} from './deep-map/index.js'
+export { keepMount } from './keep-mount/index.js'
 export {
   onAction,
   onMount,
@@ -7,6 +21,7 @@ export {
   onStop,
   STORE_UNMOUNT_DELAY
 } from './lifecycle/index.js'
+export { listenKeys } from './listen-keys/index.js'
 export {
   AnyStore,
   map,
@@ -17,11 +32,4 @@ export {
   StoreValue,
   WritableStore
 } from './map/index.js'
-export * from './deep-map/index.js'
-export { atom, Atom, ReadableAtom, WritableAtom } from './atom/index.js'
 export { allTasks, cleanTasks, startTask, task } from './task/index.js'
-export { action, lastAction } from './action/index.js'
-export { clean, cleanStores } from './clean-stores/index.js'
-export { listenKeys } from './listen-keys/index.js'
-export { keepMount } from './keep-mount/index.js'
-export { computed } from './computed/index.js'
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+export { action, lastAction } from './action/index.js'
+export { atom } from './atom/index.js'
+export { clean, cleanStores } from './clean-stores/index.js'
+export { computed } from './computed/index.js'
+export { deepMap, getPath, setPath } from './deep-map/index.js'
+export { keepMount } from './keep-mount/index.js'
 export {
   onAction,
   onMount,
@@ -7,12 +13,6 @@ export {
   onStop,
   STORE_UNMOUNT_DELAY
 } from './lifecycle/index.js'
-export { allTasks, cleanTasks, startTask, task } from './task/index.js'
-export { action, lastAction } from './action/index.js'
-export { clean, cleanStores } from './clean-stores/index.js'
 export { listenKeys } from './listen-keys/index.js'
-export { keepMount } from './keep-mount/index.js'
-export { computed } from './computed/index.js'
-export { deepMap, getPath, setPath } from './deep-map/index.js'
-export { atom } from './atom/index.js'
 export { map } from './map/index.js'
+export { allTasks, cleanTasks, startTask, task } from './task/index.js'
