78b70b3fa146848d44ac7bc1a2c48c856ed3da9f
Brian Takita <brian.takita@gmail.com>
2023-07-18 22:55:11 -0400

    Variables in tests & jsdocs match $ prefix convention (#226)
    ported from https://github.com/nanostores/nanostores/pull/221

--- action/index.d.ts
+++ action/index.d.ts
@@ -8,28 +8,28 @@ export const lastAction: unique symbol
 export const actionId: unique symbol
 
 /**
- * Action is a function which change the store.
+ * Action is a function which changes the store.
  *
  * This wrap allows DevTools to see the name of action, which changes the store.
  *
  * ```js
- * export const increase = action($counter, 'increase', (store, value = 1) => {
- *   if (validateMax(store.get() + value)) {
- *     store.set(store.get() + value)
+ * export const increase = action($counter, 'increase', ($store, value = 1) => {
+ *   if (validateMax($store.get() + value)) {
+ *     $store.set($store.get() + value)
  *   }
- *   return store.get()
+ *   return $store.get()
  * })
  *
  * increase()  //=> 1
  * increase(5) //=> 6
  * ```
  *
- * @param store Store instance.
+ * @param $store Store instance.
  * @param actionName Action name for logs.
  * @param cb Function changing the store.
  * @returns Wrapped function with the same arguments.
  */
 export function action<
   SomeStore extends WritableStore,
-  Callback extends (store: SomeStore, ...args: any[]) => any
->(store: SomeStore, actionName: string, cb: Callback): OmitFirstArg<Callback>
+  Callback extends ($store: SomeStore, ...args: any[]) => any
+>($store: SomeStore, actionName: string, cb: Callback): OmitFirstArg<Callback>
--- deep-map/index.test.ts
+++ deep-map/index.test.ts
@@ -17,11 +17,11 @@ test.after(() => {
 test('initializes store when it has listeners', () => {
   let events: string[] = []
 
-  let store = deepMap<{ a: number; b: number }>()
+  let $store = deepMap<{ a: number; b: number }>()
 
-  onMount(store, () => {
-    store.setKey('a', 0)
-    store.setKey('b', 0)
+  onMount($store, () => {
+    $store.setKey('a', 0)
+    $store.setKey('b', 0)
     events.push('init')
     return () => {
       events.push('destroy')
@@ -30,24 +30,24 @@ test('initializes store when it has listeners', () => {
 
   equal(events, [])
 
-  let unbind1 = store.listen((value, key) => {
+  let unbind1 = $store.listen((value, key) => {
     events.push(`1: ${key} ${JSON.stringify(value)}`)
   })
   equal(events, ['init'])
 
-  let unbind2 = store.listen((value, key) => {
+  let unbind2 = $store.listen((value, key) => {
     events.push(`2: ${key} ${JSON.stringify(value)}`)
   })
   equal(events, ['init'])
 
-  store.setKey('a', 1)
+  $store.setKey('a', 1)
   equal(events, ['init', '1: a {"a":1,"b":0}', '2: a {"a":1,"b":0}'])
 
   unbind1()
   clock.runAll()
   equal(events, ['init', '1: a {"a":1,"b":0}', '2: a {"a":1,"b":0}'])
 
-  store.setKey('b', 1)
+  $store.setKey('b', 1)
   equal(events, [
     'init',
     '1: a {"a":1,"b":0}',
@@ -63,7 +63,7 @@ test('initializes store when it has listeners', () => {
     '2: b {"a":1,"b":1}'
   ])
 
-  let unbind3 = store.listen(() => {})
+  let unbind3 = $store.listen(() => {})
   clock.runAll()
   equal(events, [
     'init',
@@ -93,18 +93,18 @@ test('initializes store when it has listeners', () => {
 test('supports complicated case of last unsubscribing', () => {
   let events: string[] = []
 
-  let store = deepMap<{}>()
+  let $store = deepMap<{}>()
 
-  onMount(store, () => {
+  onMount($store, () => {
     return () => {
       events.push('destroy')
     }
   })
 
-  let unbind1 = store.listen(() => {})
+  let unbind1 = $store.listen(() => {})
   unbind1()
 
-  let unbind2 = store.listen(() => {})
+  let unbind2 = $store.listen(() => {})
   unbind2()
 
   clock.runAll()
@@ -117,22 +117,22 @@ test('supports the same listeners', () => {
     events.push(`${key}: ${key ? value[key] : undefined}`)
   }
 
-  let store = deepMap<{ a: number }>()
+  let $store = deepMap<{ a: number }>()
 
-  onMount(store, () => {
+  onMount($store, () => {
     return () => {
       events.push('destroy')
     }
   })
 
-  let unbind1 = store.listen(listener)
-  let unbind2 = store.listen(listener)
-  store.setKey('a', 1)
+  let unbind1 = $store.listen(listener)
+  let unbind2 = $store.listen(listener)
+  $store.setKey('a', 1)
   equal(events, ['a: 1', 'a: 1'])
 
   unbind1()
   clock.runAll()
-  store.setKey('a', 2)
+  $store.setKey('a', 2)
   equal(events, ['a: 1', 'a: 1', 'a: 2'])
 
   unbind2()
@@ -143,21 +143,21 @@ test('supports the same listeners', () => {
 test('can subscribe to changes and call listener immediately', () => {
   let events: string[] = []
 
-  let store = deepMap<{ a: number }>()
+  let $store = deepMap<{ a: number }>()
 
-  onMount(store, () => {
-    store.setKey('a', 0)
+  onMount($store, () => {
+    $store.setKey('a', 0)
     return () => {
       events.push('destroy')
     }
   })
 
-  let unbind = store.subscribe((value, key) => {
+  let unbind = $store.subscribe((value, key) => {
     events.push(`${key}: ${JSON.stringify(value)}`)
   })
   equal(events, ['undefined: {"a":0}'])
 
-  store.setKey('a', 1)
+  $store.setKey('a', 1)
   equal(events, ['undefined: {"a":0}', 'a: {"a":1}'])
 
   unbind()
@@ -168,29 +168,29 @@ test('can subscribe to changes and call listener immediately', () => {
 test('supports starting store again', () => {
   let events: string[] = []
 
-  let store = deepMap<{ a: number }>()
+  let $store = deepMap<{ a: number }>()
 
-  onMount(store, () => {
-    store.setKey('a', 0)
+  onMount($store, () => {
+    $store.setKey('a', 0)
     events.push('init')
     return () => {
       events.push('destroy')
     }
   })
 
-  let unbind = store.subscribe(value => {
+  let unbind = $store.subscribe(value => {
     events.push(`${value.a}`)
   })
 
-  store.setKey('a', 1)
+  $store.setKey('a', 1)
 
   unbind()
   clock.runAll()
 
-  store.set({ a: 2 })
-  store.setKey('a', 3)
+  $store.set({ a: 2 })
+  $store.setKey('a', 3)
 
-  store.subscribe(value => {
+  $store.subscribe(value => {
     events.push(`${value.a}`)
   })
   equal(events, ['init', '0', '1', 'destroy', 'init', '0'])
@@ -199,14 +199,14 @@ test('supports starting store again', () => {
 test('works without initializer', () => {
   let events: (string | undefined)[] = []
 
-  let store = deepMap<{ a: number }>()
+  let $store = deepMap<{ a: number }>()
 
-  let unbind = store.subscribe((value, key) => {
+  let unbind = $store.subscribe((value, key) => {
     events.push(key)
   })
   equal(events, [undefined])
 
-  store.setKey('a', 1)
+  $store.setKey('a', 1)
   equal(events, [undefined, 'a'])
 
   unbind()
@@ -217,9 +217,9 @@ test('supports conditional destroy', () => {
   let events: string[] = []
 
   let destroyable = true
-  let store = deepMap<{ one?: number }>()
+  let $store = deepMap<{ one?: number }>()
 
-  onMount(store, () => {
+  onMount($store, () => {
     events.push('init')
     if (destroyable) {
       return () => {
@@ -228,101 +228,101 @@ test('supports conditional destroy', () => {
     }
   })
 
-  let unbind1 = store.listen(() => {})
+  let unbind1 = $store.listen(() => {})
   unbind1()
   clock.runAll()
   equal(events, ['init', 'destroy'])
 
   destroyable = false
-  let unbind2 = store.listen(() => {})
+  let unbind2 = $store.listen(() => {})
   unbind2()
   clock.runAll()
   equal(events, ['init', 'destroy', 'init'])
 })
 
 test('changes the whole object', () => {
-  let store = deepMap<{ a: number; b: number; c?: number }>()
+  let $store = deepMap<{ a: number; b: number; c?: number }>()
 
-  onMount(store, () => {
-    store.setKey('a', 0)
-    store.setKey('b', 0)
+  onMount($store, () => {
+    $store.setKey('a', 0)
+    $store.setKey('b', 0)
   })
 
   let changes: (string | undefined)[] = []
-  store.listen((value, key) => {
+  $store.listen((value, key) => {
     changes.push(key)
   })
 
-  store.set({ a: 1, b: 0, c: 0 })
-  equal(store.get(), { a: 1, b: 0, c: 0 })
+  $store.set({ a: 1, b: 0, c: 0 })
+  equal($store.get(), { a: 1, b: 0, c: 0 })
   equal(changes, [undefined])
 
-  store.set({ a: 1, b: 1 })
-  equal(store.get(), { a: 1, b: 1 })
+  $store.set({ a: 1, b: 1 })
+  equal($store.get(), { a: 1, b: 1 })
   equal(changes, [undefined, undefined])
 })
 
 test('does not call listeners on no changes', () => {
-  let store = deepMap<{ one: number }>({ one: 1 })
+  let $store = deepMap<{ one: number }>({ one: 1 })
 
   let changes: (string | undefined)[] = []
-  store.listen((value, key) => {
+  $store.listen((value, key) => {
     changes.push(key)
   })
 
-  store.setKey('one', 1)
-  store.set({ one: 1 })
+  $store.setKey('one', 1)
+  $store.set({ one: 1 })
   equal(changes, [undefined])
 })
 
 test('changes value object reference', () => {
-  let store = deepMap<{ a: number }>({ a: 0 })
+  let $store = deepMap<{ a: number }>({ a: 0 })
 
   let checks: boolean[] = []
   let prev: { a: number } | undefined
-  store.subscribe(value => {
+  $store.subscribe(value => {
     if (prev) checks.push(value === prev)
     prev = value
   })
 
-  store.setKey('a', 1)
-  store.set({ a: 2 })
+  $store.setKey('a', 1)
+  $store.set({ a: 2 })
   equal(checks, [false, false])
 })
 
 test('deletes keys on undefined value', () => {
-  let store = deepMap<{ a: number | undefined }>()
+  let $store = deepMap<{ a: number | undefined }>()
 
   let keys: string[][] = []
-  store.listen(value => {
+  $store.listen(value => {
     keys.push(Object.keys(value))
   })
 
-  store.setKey('a', 1)
-  store.setKey('a', undefined)
+  $store.setKey('a', 1)
+  $store.setKey('a', undefined)
   equal(keys, [['a'], []])
 })
 
 test('does not mutate listeners while change event', () => {
   let events: string[] = []
-  let store = deepMap<{ a: number }>({ a: 0 })
+  let $store = deepMap<{ a: number }>({ a: 0 })
 
-  store.listen(value => {
+  $store.listen(value => {
     events.push(`a${value.a}`)
     unbindB()
-    store.listen(v => {
+    $store.listen(v => {
       events.push(`c${v.a}`)
     })
   })
 
-  let unbindB = store.listen(value => {
+  let unbindB = $store.listen(value => {
     events.push(`b${value.a}`)
   })
 
-  store.setKey('a', 1)
+  $store.setKey('a', 1)
   equal(events, ['a1', 'b1'])
 
-  store.setKey('a', 2)
+  $store.setKey('a', 2)
   equal(events, ['a1', 'b1', 'a2', 'c2'])
 })
 
