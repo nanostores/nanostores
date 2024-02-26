09e6644c1a953e9e42a14e6b26893d95d8bc5aa5
Lars Johansson <lars.johansson@ftrack.com>
2024-02-26 21:08:56 +0100

    feat: add subscribeKeys function (#275)
    * feat: add subscribeKeys function
    
    * Update size limit
    
    * Update listen-keys/index.d.ts
    
    Co-authored-by: Andrey Sitnik <andrey@sitnik.ru>
    
    * Update listen-keys/index.js
    
    Co-authored-by: Andrey Sitnik <andrey@sitnik.ru>
    
    * Formatting reversal readme
    
    ---------
    
    Co-authored-by: Andrey Sitnik <andrey@sitnik.ru>

--- listen-keys/index.d.ts
+++ listen-keys/index.d.ts
@@ -3,11 +3,15 @@ import type { StoreValue } from '../map/index.js'
 /**
  * Listen for specific keys of the store.
  *
+ * In contrast with {@link subscribeKeys} it do not call listener
+ * immediately.
  * ```js
  * import { listenKeys } from 'nanostores'
  *
- * listenKeys($page, ['blocked'], () => {
- *   console.log('You has no access')
+ * listenKeys($page, ['blocked'], (value, oldValue, changed) => {
+ *   if (value.blocked) {
+ *     console.log('You has no access')
+ *   }
  * })
  * ```
  *
@@ -25,6 +29,42 @@ export function listenKeys<
   listener: (
     value: StoreValue<SomeStore>,
     oldValue: StoreValue<SomeStore>,
+    changed: SomeStore extends {
+      setKey: (key: infer Key, value: never) => unknown
+    }
+      ? Key[]
+      : never
+  ) => void
+): () => void
+
+/**
+ * Listen for specific keys of the store and call listener immediately.
+ * Note that the oldValue and changed arguments in the listener are
+ * undefined during the initial call.
+ *
+ * ```js
+ * import { subscribeKeys } from 'nanostores'
+ *
+ * subscribeKeys($page, ['blocked'], (value, oldValue, changed) => {
+ *   if (value.blocked) {
+ *     console.log('You has no access')
+ *   }
+ * })
+ * ```
+ *
+ * @param $store The store to listen.
+ * @param keys The keys to listen.
+ * @param listener Standard listener.
+ */
+export function subscribeKeys<
+  SomeStore extends { setKey: (key: any, value: any) => void }
+>(
+  $store: SomeStore,
+  keys: SomeStore extends { setKey: (key: infer Key, value: never) => unknown }
+    ? readonly Key[]
+    : never,
+  listener: (
+    value: StoreValue<SomeStore>,
     changed: SomeStore extends {
       setKey: (key: infer Key, value: never) => unknown
     }
