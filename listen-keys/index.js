e79bc1670f74fa8df5227372b67ee80bdccbdd78
Vladimir Ivakin <vla.ivakin@gmail.com>
2024-09-10 21:36:55 +0200

    Avoid creating of the new array in the listenKeys function

--- listen-keys/index.js
+++ listen-keys/index.js
@@ -1,5 +1,5 @@
 export function listenKeys($store, keys, listener) {
-  let keysSet = new Set([undefined, ...keys])
+  let keysSet = new Set(keys).add(undefined)
   return $store.listen((value, oldValue, changed) => {
     if (keysSet.has(changed)) {
       listener(value, oldValue, changed)
